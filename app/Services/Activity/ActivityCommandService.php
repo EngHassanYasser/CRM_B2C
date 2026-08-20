f84f0016268924e40bd7d89380361e0f54c4ab86
hassan <prohyatm@gmail.com>
2026-08-20 20:31:29 +0300

    defin ActivityQueryService && ActivityCommandService

---
+++ app/Services/Activity/ActivityCommandService.php
@@ -0,0 +1,14 @@
+<?php
+namespace App\Services\Activity;
+
+class ActivityCommandService {
+    public function create(array $data) {
+
+    }
+    public function update(array $data,int $activityId) {
+
+    }
+    public function deleteById(int $activityId) {
+
+    }
+}
---
+++ app/Services/Activity/ActivityQueryService.php
@@ -0,0 +1,11 @@
+<?php
+namespace App\Services\Activity;
+
+class ActivityQueryService {
+    public function getStats() {
+
+    }
+    public function getActivities() {
+
+    }
+}
